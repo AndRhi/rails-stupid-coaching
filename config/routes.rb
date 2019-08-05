63c9e7d233e2788f54627f78b52938f3a9ddc730
Andrea Rhiel <andrea.rhiel@yahoo.de>
2019-08-05 14:51:11 +0100

    ..

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'questions', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+# get 'about', to: 'pages#about', as: :about
+#   get 'contact', to: 'pages#contact', as: :contact
+
+  # Generic syntax:
+  # verb 'path', to: 'controller#action', as: :route_name
